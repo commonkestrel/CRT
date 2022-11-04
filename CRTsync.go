ad44913b6eb8f7b11a22e728a7a2552f5dfbae9f
Replit user <>
2022-11-04 08:03:32 -0700

    refactor to remove bugs

---
+++ CRTsync.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+    "image/color"
+    "log"
+    "time"
+
+    "example/CRT/neopixel"
+    "example/CRT/unpack"
+
+    "github.com/chbmuc/lirc"
+)
+
+var (
+    rgb      = color.RGBA{255, 255, 255, 255}
+    change = make(chan bool)
+    matrix   *neopixel.Matrix
+
+    BLACK = color.RGBA{0, 0, 0, 0}
+    WHITE = color.RGBA{255, 255, 255, 255}
+
+    fn int
+    anim *unpack.Anim
+)
+
+const (
+    WIDTH, HEIGHT = 15, 11
+)
+
+func event(event lirc.Event) {
+    if event.Repeat > 0 {
+        return
+    }
+
+    newanim, err := unpack.Search(event.Button)
+    if err != nil {
+        if unpack.IsNotFound(err) {
+            log.Printf("Key %v not registered\n", event.Button)
+            return
+        }
+        log.Println(err)
+        return
+    }
+
+    anim = newanim
+    change <- true
+}
+
+func main() {
+    var err error
+    matrix, err = neopixel.NewMatrix(WIDTH, HEIGHT, 21)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer matrix.Close()
+
+    ir, err := lirc.Init("/var/run/lirc/lircd")
+    if err != nil {
+        panic(err)
+    }
+    ir.Handle("", "", event)
+    matrix.Set(0, 0, color.White)
+    matrix.Set(WIDTH-1, 0, color.White)
+    matrix.Set(0, HEIGHT-1, color.White)
+    matrix.Set(WIDTH-1, HEIGHT-1, color.White)
+    matrix.Render()
+
+    go ir.Run()
+    log.Println("running")
+    run()
+}
+
+func run() {
+    <-change
+    out:
+    for {
+        select {
+        case <-change:
+            fn = 0
+        default:
+        }
+
+        fps := time.NewTicker(time.Second/time.Duration(anim.Fps))
+
+        marcher:
+        for fn < len(anim.Frames) {
+            select {
+            case <-change:
+                continue out
+            default:
+            }
+
+            frame := anim.Frames[fn]
+
+            if len(frame) == 0 {
+                timer := time.NewTimer(anim.LoopDelay)
+
+                select {
+                case <-timer.C:
+                    fn++
+                    timer.Stop()
+                    fps.Reset(time.Second/time.Duration(anim.Fps))
+                    continue marcher
+                case <-change:
+                    timer.Stop()
+                    continue out
+                }
+            }
+
+            frame.Draw(matrix)
+            matrix.Render()
+            fn++
+
+            select {
+            case <-fps.C:
+                continue marcher
+            case <-change:
+                continue out
+            }
+        }
+        if anim.Loop {
+            fn = 0
+            if anim.LoopDelay > 0 {
+                timer := time.NewTicker(time.Duration(anim.LoopDelay))
+                select {
+                case <-timer.C:
+                    timer.Stop()
+                    continue out
+                case <-change:
+                    timer.Stop()
+                    continue out
+                }
+            }
+        }
+    }
+}
--- unpack/unpack.go
+++ unpack/unpack.go
@@ -1,49 +1,98 @@
 package unpack
 
 import (
-    "os"
-    "time"
-    "image/color"
-    "encoding/json"
+	"encoding/json"
+	"errors"
+	"image/color"
+	"os"
+	"strings"
+	"time"
 
-    "example/CRT/neopixel"
+	"example/CRT/neopixel"
 )
+
+var errNotFound = errors.New("command not found for key.")
+var keys = map[string]string{"power": "KEY_POWER", "volumeup": "KEY_VOLUMEUP", "stop": "KEY_STOP", "previous": "KEY_PREVIOUS", "playpause": "KEY_PLAYPAUSE", "next": "KEY_NEXT", "down": "KEY_DOWN", "volumedown": "KEY_VOLUMEDOWN", "up": "KEY_UP", "equal": "KEY_EQUAL", "start": "BTN_START", "1": "KEY_1", "2": "KEY_2", "3": "KEY_3", "4": "KEY_4", "5": "KEY_5", "6": "KEY_6", "7": "KEY_7", "8": "KEY_8", "9": "KEY_9", "0": "KEY_0"}
 
 const (
     WIDTH, HEIGHT = 15, 11
 )
 
 type Anim struct {
-	Frames []Frame
+    Frames []Frame
     Fps int
     Loop bool
     LoopDelay time.Duration
 }
 
-type Frame [WIDTH][HEIGHT]color.RGBA
+type frames struct {
+    Frames []Frame
+    Fps int
+}
+
+type in struct {
+    Name string
+    Filename string
+    Key string
+    Loop bool
+    LoopDelay int //In milliseconds
+}
+
+type Frame [][]color.RGBA
 
 func (f *Frame) Draw(mat *neopixel.Matrix) {
-    for x, col := range f {
+    for x, col := range *f {
         for y, rgb := range col {
             mat.Set(x, mat.Height-(y+1), rgb)
         }
     }
 }
 
-func UnpackFile(path string) (*Anim, error) {
-    var unpacked Anim
-    file, err := os.ReadFile(path)
+func readindex() ([]in, error) {
+    file, err := os.ReadFile("./store/index.json")
     if err != nil {
         return nil, err
     }
-    json.Unmarshal([]byte(file), &unpacked)
 
-	var newframes []Frame
-	for _, f := range unpacked.Frames {
-		newframes = append([]Frame{f}, newframes...)
-	}
+    var unmarshaled []in
+    err = json.Unmarshal(file, &unmarshaled)
+    if err != nil {
+        return nil, err
+    }
 
-    return &unpacked, nil
+    return unmarshaled, nil
 }
 
+func Search(key string) (*Anim, error) {
+    index, err := readindex()
+    if err != nil {
+        return nil, err
+    }
 
+    var found *in = nil
+    for _, c := range index {
+        if keys[strings.ToLower(c.Key)] == key {
+            found = &c
+        }
+    }
+    if found == nil {
+        return nil, errNotFound
+    }
+
+    path := found.Filename
+
+    var unmarshaled frames
+    file, err := os.ReadFile("./store/" + path)
+    if err != nil {
+        return nil, err
+    }
+    json.Unmarshal([]byte(file), &unmarshaled)
+
+    unpacked := &Anim{unmarshaled.Frames, unmarshaled.Fps, found.Loop, time.Duration(found.LoopDelay)*time.Millisecond}
+
+    return unpacked, nil
+}
+
+func IsNotFound(err error) bool {
+    return err == errNotFound
+}
